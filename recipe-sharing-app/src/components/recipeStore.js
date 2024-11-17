4bd057f18a9ad90e39da82a357b044f200060c91
Oussama JAMIL <oussamajamil321@gmail.com>
2024-11-17 13:08:06 +0100

    m

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,9 +1,9 @@
 import { create } from 'zustand'
 
-const useRecipeStore = create(set => ({
+const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
+  setRecipes: (recipes) => set({ recipes }),
 }));
 
  export default useRecipeStore;
