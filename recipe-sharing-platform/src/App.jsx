268422df7fdf540b1722796c4ee23bc099edc2fc
Oussama JAMIL <oussamajamil321@gmail.com>
2024-12-08 20:08:43 +0100

    m

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-blue-500 text-center">
+      <h1>Welcome to the Recipe Sharing Platform!</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
