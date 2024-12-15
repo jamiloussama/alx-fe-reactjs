31073ff05280e3f4312dd4ecb31a7f0c050ed23d
Oussama JAMIL <oussamajamil321@gmail.com>
2024-12-15 21:35:14 +0100

    m

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,15 +1,28 @@
 import axios from 'axios';
 
-const GITHUB_API_BASE_URL = 'https://api.github.com';
+const BASE_URL = 'https://api.github.com';
 
-const fetchAdvancedSearchResults = async ({ username, location, minRepos }) => {
-  let query = '';
-  if (username) query += `${username} in:login`;
-  if (location) query += ` location:${location}`;
-  if (minRepos) query += ` repos:>=${minRepos}`;
-
-  const response = await axios.get(`${GITHUB_API_BASE_URL}/search/users?q=${query}`);
-  return response.data;
+// Fetch user data by username
+export const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/users/${username}`);
+    return response.data;
+  } catch (error) {
+    throw new Error('Error fetching user data');
+  }
 };
 
-export { fetchAdvancedSearchResults };
+// Fetch users with advanced search criteria
+export const fetchAdvancedSearchResults = async ({ username, location, minRepos }) => {
+  try {
+    let query = '';
+    if (username) query += `user:${username}`;
+    if (location) query += ` location:${location}`;
+    if (minRepos) query += ` repos:>${minRepos}`;
+
+    const response = await axios.get(`https://api.github.com/search/users?q=${encodeURIComponent(query)}`);
+    return response.data;
+  } catch (error) {
+    throw new Error('Error fetching advanced search results');
+  }
+};
