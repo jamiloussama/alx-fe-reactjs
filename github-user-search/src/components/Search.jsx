ecd857fee257e4011d72b583f790951c2b7db276
Oussama JAMIL <oussamajamil321@gmail.com>
2024-12-15 21:18:07 +0100

    m

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -21,7 +21,7 @@ const Search = () => {
       const data = await fetchUserData(searchTerm);
       setUserData(data);
     } catch (err) {
-      setError("Looks like we can't find the user");
+      setError("Looks like we cant find the user");
     } finally {
       setLoading(false);
     }
