f0499942f2c5b70c0ea5bbd18a1724d308756427
Oussama JAMIL <oussamajamil321@gmail.com>
2024-12-15 21:28:16 +0100

    m

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,10 +1,15 @@
 import axios from 'axios';
 
-const GITHUB_API_BASE_URL = 'https://api.github.com/users';
+const GITHUB_API_BASE_URL = 'https://api.github.com';
 
-const fetchUserData = async (username) => {
-  const response = await axios.get(`${GITHUB_API_BASE_URL}/${username}`);
+const fetchAdvancedSearchResults = async ({ username, location, minRepos }) => {
+  let query = '';
+  if (username) query += `${username} in:login`;
+  if (location) query += ` location:${location}`;
+  if (minRepos) query += ` repos:>=${minRepos}`;
+
+  const response = await axios.get(`${GITHUB_API_BASE_URL}/search/users?q=${query}`);
   return response.data;
 };
 
-export { fetchUserData };
+export { fetchAdvancedSearchResults };
